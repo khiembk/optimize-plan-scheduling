f239985b810fb3396d9897f461f8623cebf6f0f0
trankhiem <trankhiem2003@gmail.com>
2025-03-03 17:58:16 +0700

    updatesimplex1

--- simplex_method.py
+++ simplex_method.py
@@ -28,20 +28,80 @@ def read_input():
     return n, m, C, A, b
 
 def convertToStandard(m,n, matrix_inequation, b_inequation):
-    for index in range(n):
+    for index in range(m):
         cur_row = matrix_inequation[index]
-        for j in range(n):
+        for j in range(m):
             cur_row.append(0)
 
-        cur_row[m + index] = 1
+        cur_row[n + index] = 1
         matrix_inequation[index] = cur_row
     
-    return matrix_inequation
+    return matrix_inequation, b_inequation
             
-    
+def InitLastRow(c, m,n):
+    lastRow = (m+n)*[0]
+    for i in range(n): 
+        lastRow[i] = -c[i]
+
+    return lastRow    
+
+def find_min_negative_column_lr(lastRow):
+    min_value = 0
+    min_index = -1
+    for index in range(lastRow):
+        if (lastRow[index] < min_value):
+                min_value = lastRow[index]
+                min_index = index
+
+    return min_index
+
+def standardlize_row_with_index(cur_row, rhs, index):
+    standard_value = cur_row[index]
+    for i in range(len(cur_row)):
+        cur_row[i] = cur_row[i]/standard_value
+
+    rhs = rhs/standard_value
+
+    return cur_row, rhs
+
+def Calculate_estimate(matrix_euqation, index, rhs):
+    estimation = []
+    for i in range(len(matrix_equation)): 
+        row = matrix_equation[i]
+        if row[index] <= 0: 
+            estimation.append(float('inf'))
+        else: 
+            estimation.append(rhs[i]/ row[index])    
+
+    return estimation
+
+def find_row_pivot(estimation):
+    index = -1
+    max_value = float('inf')
+    for i in range(len(estimation)):
+        if (estimation[i] < max_value):
+            index = i
+            max_value = estimation[i]
+
+    return index
+
+def linear_transform_row(cur_row, pivot_index, pivot_row, rsh, cur_row_index, rsh_pivot):
+    scale_value = cur_row[pivot_index]
+    rsh[cur_row_index] = rsh[cur_row_index] - scale_value*rsh_pivot
+    for i in range(len(cur_row)):
+        cur_row[i] = cur_row[i] - scale_value*pivot_row[i]
+
+    return cur_row, rsh
+
+def update_last_row(cur_lastrow, pivot_row, rsh_pivot, pivot_index):
+    return 0    
+
 if __name__ == "__main__":
-    m, n, c, matrix_inequation, b_inequation = read_input()
-    matrix_equation = convertToStandard(m,n, matrix_inequation, b_inequation)
-    print(matrix_equation)
+    n, m, c, matrix_inequation, b_inequation = read_input()
+    matrix_equation, b_equation = convertToStandard(m,n, matrix_inequation, b_inequation)
+    #print(matrix_equation)
+    lastRow = InitLastRow(c,m,n)
+    cur_result = 0 
+
 
      
